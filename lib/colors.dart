8522caa3e83a633d0b3d2f250dab25c587d0c068
tatxha <tatthamaharany@gmail.com>
2024-08-16 19:31:43 +0700

    add colorscheme and fonts

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+final Color primaryBackgrond = Color(0xFFFFFFFF);
+final Color secondaryBackgrond = Color(0xFFEEFFF2);
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,48 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
       ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFFEEFFF2), 
+          onPrimary: const Color(0xFF2E694B), 
+          onPrimaryContainer: const Color(0xFFBDD7C3),
+          secondary: const Color(0xFF67A076), 
+          tertiary: const Color(0xFFFE9F6A),
+          onTertiary: const Color(0xFFF5BE9F)
+        ),
+        fontFamily: 'Montserrat',
+        scaffoldBackgroundColor: Colors.white,
+        dividerColor: Color(0xFFBDD7C3), // untuk divider pada container create account dan login
+        splashColor: Color(0xFF67A076), // untuk 3 screen awal
+      ),
     );
   }
+}
+
+mixin AppMixin{
+  FontWeight bold = FontWeight.w700;
+  FontWeight semi = FontWeight.w600;
+  FontWeight medium = FontWeight.w500;
+  FontWeight regular = FontWeight.w400;
+  Color primaryIcon = Color(0xFF2E694B);
+  Color secondaryIcon = Color(0xFFFFC107);
+  Color primaryContainer = Color(0xFFEEFFF2);
+  Color secondaryContainer = Color(0xFFF4F3F2);
 }
